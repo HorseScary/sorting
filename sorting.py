ccaee94006158b1adf35d4efac85f7c9649e96a1
Vi <horsescary@gmail.com>
2022-11-10 11:38:36 -0500

    wrote a sorting algorithm

--- sorting.py
+++ sorting.py
@@ -4,12 +4,20 @@ sorted_list = [1,2,3,4,5]
 def is_sorted(list:list):
     for i in range(len(list)):
         for j in range (len(list)-i-1):
-            print(f"i:   {list[i]}\ni+j: {list[i+j+1]}")
             if list[i] > list[i+j+1]:
                 return(False)
-
     return(True)
 
+def something_sort(list:list):
+    while not is_sorted(list):
+        for i in range(len(list)):
+            print(list)
+            try:
+                if list[i] > list[i+1]:
+                    list.insert(i,list.pop(i+1))
+            except IndexError:
+                pass
+    return(list)
 
 if __name__ == "__main__":
-    print(is_sorted(sorted_list))
+    print(something_sort(unsorted_list))
